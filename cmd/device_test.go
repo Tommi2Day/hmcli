97b51fa359c77901b9145d8d4e19fe4d470779ec
Tommi2Day <tommi2day@tommi2day.net>
2024-05-25 14:03:18 +0200

    fix: linter

--- cmd/device_test.go
+++ cmd/device_test.go
@@ -1,8 +1,9 @@
 package cmd
 
 import (
-	"github.com/tommi2day/check_hm/test"
 	"testing"
+
+	"github.com/tommi2day/check_hm/test"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/jarcoal/httpmock"
